2cc6087bbd12250344982322ca332e901557fbda
Patrick Daly <patrick@developdaly.com>
2012-09-20 21:08:53 -0500

    Added development error reporting to the sample local config

--- local-config-sample.php
+++ local-config-sample.php
@@ -10,3 +10,16 @@ define( 'DB_NAME', 'local-db-name' );
 define( 'DB_USER', 'local-db-user' );
 define( 'DB_PASSWORD', 'local-db-password' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Disable display of errors and warnings 
+define( 'WP_DEBUG_DISPLAY', true );
+@ini_set( 'display_errors', 1 );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', false );
